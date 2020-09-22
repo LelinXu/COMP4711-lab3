d52403243359bba4b7c1deed4cd43605046bcf5e
11918 <11918@Xu>
2020-09-22 16:32:05 +0800

    Calculate a Grades Average Add a Text Representation

--- student.php
+++ student.php
@@ -15,5 +15,20 @@
     function add_grade($grade) {
        $this->grades[] = $grade;  
     }
+    function average() {
+        $total = 0; 
+        foreach ($this->grades as $value) 
+          $total += $value;  
+        return $total / count($this->grades); 
+    }
+    function toString() { 
+        $result = $this->first_name . ' ' . $this->surname; 
+        $result .= ' ('.$this->average().")\n"; 
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n"; 
+        $result .= "\n"; 
+        $result .="";
+        return '<pre>'.$result.'</pre>'; 
+    }
         }
         ?>
